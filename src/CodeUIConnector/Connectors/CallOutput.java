d86662a3590c5d4f52df8fec02ccbb867168595f
Morai <Morai@DESKTOP-AIJR5U1.lan>
2015-09-18 06:01:19 +0100

    Code  improvement

---
+++ src/CodeUIConnector/Connectors/CallOutput.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CodeUIConnector.Connectors;
+
+import CodeUIConnector.SocketPane.UISocket;
+
+
+
+public class CallOutput {
+    
+    private final UISocket uiSocket;
+    private CallInput connectSocket;
+    
+    
+    
+    public CallOutput(String name) {
+
+        this.uiSocket = new UICallSocket(UICallSocket.Type.OUTPUT,5,name);
+        
+        
+    }
+
+    public void setConnect(CallInput connectSocket) {
+        this.connectSocket = connectSocket;
+    }
+
+    public CallInput getConnectSocket() {
+        return connectSocket;
+    }
+
+    public UISocket getUISocket() {
+        return uiSocket;
+    }
+
+    
+}
